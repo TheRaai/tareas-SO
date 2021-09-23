28ba6c16e685e5887f7aba9632e78201dd4e2cb8
TheRaai <raimundomoraga@hotmail.com>
2021-09-22 21:05:09 -0300

    tarea 3 finished

--- tarea3.c
+++ tarea3.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include<stdbool.h>
 
 typedef struct node{
     int data;
@@ -81,24 +82,32 @@ node* remove_back(node* head)
  
     return head;
 }
+
 typedef struct{
   node* ListHead;
   int value;
 }argumentos;
+
+typedef struct{
+    pthread_t thread;
+}threads;
+
 void* append_thread(void* arg)
 {
     argumentos *a = (argumentos*) arg;
     node *prev = a->ListHead;
-    pthread_mutex_lock(&prev->lock);
     while (prev->next != NULL) {
       prev= prev->next;
     }
-    pthread_mutex_unlock(&prev->lock);
+    pthread_mutex_lock(&prev->lock);
     node* new_node =  create(a->value,NULL);
     prev->next = new_node;
-    
+    pthread_mutex_unlock(&prev->lock);
+    //pthread_exit((void*)prev);
     return(void*)a->ListHead;
 }
+
+
 int main(void) {
   int counter=0,counter2=0;
   node* head = create(0,NULL);
@@ -114,22 +123,25 @@ int main(void) {
     temp=temp->next;
   }
   argumentos b[100];
-  node* head2 = create(0,NULL);
+  node head2 = {0,0};
+  pthread_t p;
   for(int i =1;i<100;i++){
-    pthread_t p;
-    b[i].ListHead = head2;
-    pthread_mutex_init(&b[i].ListHead->lock,NULL);
+    //pthread_mutex_init(&b[i].ListHead->lock,0);
+    b[i].ListHead = &head2;
     b[i].value = i;
     pthread_create(&p,NULL,append_thread,&b[i]);
+    pthread_join(p,NULL);
   }
-  node* temp2  = head2;
+
+  node* temp2  = &head2;
   while(temp2!=NULL){
     counter2++;
     temp2=temp2->next;
   }
-  printf("Lista de largo: %d\n",counter);
+  printf("Lista hecha con while de largo: %d\n",counter);
   print_list(head);
-  printf("\nListo! lista con largo: %d\n",counter2);
-  print_list(head2);
+  printf("\nLista hecha con threading con largo: %d\n",counter2);
+  print_list(&head2);
+  printf("\n");
   return 0;
 }
